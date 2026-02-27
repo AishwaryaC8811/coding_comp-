3edc9fa1ebeaf96758a7a40fe28b9727004ff148
Aishwarya C <aishwaryachandalada@gmail.com>
2026-02-27 17:27:02 +0000

    challenge

---
+++ 1-test-cases.py
@@ -0,0 +1,26 @@
+import sys
+import math
+import string
+# Read number of test cases from stdin.
+Test_line = sys.stdin.readline().rstrip() # Default is a STRING
+
+T = int(Test_line)
+print("number of test cases:", T)
+
+for i in range(T):
+    # Read the next line from standard input.
+    # Each test case is assumed to contain two integers seperated by whotespace.
+
+     # Split the line into separate string tokens.
+
+    # Convert string tokens into integers using map().
+    # map() is lazy, meaning conversion happens during iteration.
+
+    # Compute the required outputs.
+    # compute the total of both numbers added together
+    # compute the product of both numbers
+
+    # print result
+    T = sys.stdin.readline().rstrip()
+    
+   
